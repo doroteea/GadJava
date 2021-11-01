7540307a657359a450db92f51d50b250abb14c3c
DORO <56334333+doroteea@users.noreply.github.com>
2021-11-01 21:01:57 +0200

    coffee

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -2,6 +2,9 @@ package lab1;
 
 public class Main {
     public static void main(String[] args) {
+        Exercises ex = new Exercises();
+        ex.arrays();
         System.out.println("Atelier digital Google!");
     }
 }
+
